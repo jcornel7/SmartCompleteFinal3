078354160bcc52714d3a85a98aa849245b7cc61d
jcornel7 <jcornel7@users.noreply.github.com>
2017-08-18 12:10:02 -0400

    bigquery update

--- www/search_bq.php
+++ www/search_bq.php
@@ -17,7 +17,7 @@ if(isset($_GET['term'])){
             'projectId' => $projectId,
         ]);
 
-    $query = "SELECT * FROM [wise-hub-175615:gcp.products] where product like '%{$term}%'";
+    $query = "SELECT * FROM [smartcompletedemo-v2:products.name] where product like '%{$term}%'";
     $options = ['useLegacySql' => true];
     $queryResults = $bigQuery->runQuery($query, $options);
 
