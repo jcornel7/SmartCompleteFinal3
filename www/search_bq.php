681310f2a9d31de2d22682e8cb34638284513529
jcornel7 <jcornel7@users.noreply.github.com>
2017-08-18 13:47:40 -0400

    php legacy

--- www/search_bq.php
+++ www/search_bq.php
@@ -12,12 +12,12 @@ if(isset($_GET['term'])){
 
 	$term = $_GET['term'];
 
-	$projectId = "smartcompletedemo-v2";
+	$projectId = "wise-hub-175615";
 	$bigQuery = new BigQueryClient([
             'projectId' => $projectId,
         ]);
 
-    $query = "SELECT * FROM [smartcompletedemo-v2:products.name] where product like '%{$term}%'";
+    $query = "SELECT * FROM [wise-hub-175615:gcp.products] where product like '%{$term}%'";
     $options = ['useLegacySql' => true];
     $queryResults = $bigQuery->runQuery($query, $options);
 
